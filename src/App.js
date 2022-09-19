e8fa56e5a0389975b96e5dfa5afaeed65a02b9ad
name <gaga_1000@naver.com>
2022-09-19 18:31:29 +0900

    '부트스트랩 설치'

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { Button } from "bootstrap";
 
 function App() {
   return (
     <div className="App">
       <p>집갈래</p>
+      <Button>버튼</Button>
       <p>어려운 git</p>
     </div>
   );
