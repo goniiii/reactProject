81a5eb53bed3b257a1dd2744438e8c2c095bdd87
name <gaga_1000@naver.com>
2022-09-19 18:46:36 +0900

    App.js 에 import route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { Button } from "bootstrap";
+import { Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <p>집갈래</p>
       <Button>버튼</Button>
+      <Route></Route>
       <p>어려운 git</p>
     </div>
   );
