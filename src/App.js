0ffc290ad6ff7d5780fb539e5c729f36a1cfb7bb
name <gaga_1000@naver.com>
2022-09-20 02:20:36 +0900

    routes 추가

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
 import { Button } from "bootstrap";
-import { Route } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <p>집갈래</p>
       <Button>버튼</Button>
-      <Route></Route>
+      <Routes>
+        <Route></Route>
+      </Routes>
       <p>어려운 git</p>
     </div>
   );
